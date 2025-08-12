62226d2116ce9dc458725aa90762a2aeb6cef4ef
LucasAugustusvd <125266388+LucasAugustusvd@users.noreply.github.com>
2025-08-12 14:48:16 +0200

    Add files via upload

---
+++ Full_state/models_decoupled.py
@@ -0,0 +1,162 @@
+import tensorflow_quantum as tfq
+
+import cirq
+import sympy
+import tensorflow as tf
+from tensorflow.keras import layers
+import numpy as np
+
+def one_qubit_rotation(qubit, symbols):
+    """
+    Returns Cirq gates that apply a rotation of the bloch sphere about the X,
+    Y and Z axis, specified by the values in `symbols`.
+    """
+    return [cirq.rx(symbols[0])(qubit),
+            cirq.ry(symbols[1])(qubit),
+            cirq.rz(symbols[2])(qubit)]
+
+def entangling_layer(qubits):
+    """
+    Returns a layer of CZ entangling gates on `qubits` (arranged in a circular topology).
+    """
+    cz_ops = [cirq.CZ(q0, q1) for q0, q1 in zip(qubits, qubits[1:])]
+    #cz_ops += ([cirq.CZ(qubits[0], qubits[-1])] if len(qubits) != 2 else [])
+    return cz_ops
+
+def generate_circuit(qubits, n_layers):
+    """Prepares a data re-uploading circuit on `qubits` with `n_layers` layers."""
+    # Number of qubits
+    n_qubits = len(qubits)
+
+    # Sympy symbols for variational angles
+    params = sympy.symbols(f'theta(0:{3*(n_layers+1)*n_qubits})')
+    params = np.asarray(params).reshape((n_layers + 1, n_qubits, 3))
+
+    # Sympy symbols for encoding angles
+    inputs = sympy.symbols(f'x(0:{n_layers})'+f'_(0:{n_qubits})')
+    inputs = np.asarray(inputs).reshape((n_layers, n_qubits))
+
+    # Define circuit
+    circuit = cirq.Circuit()
+    for l in range(n_layers):
+        # Variational layer
+        circuit += cirq.Circuit(one_qubit_rotation(q, params[l, i]) for i, q in enumerate(qubits))
+        circuit += entangling_layer(qubits)
+        # Encoding layer
+        circuit += cirq.Circuit(cirq.rx(inputs[l, i])(q) for i, q in enumerate(qubits))
+
+    # Last varitional layer
+    circuit += cirq.Circuit(one_qubit_rotation(q, params[n_layers, i]) for i,q in enumerate(qubits))
+
+    return circuit, list(params.flat), list(inputs.flat)
+
+class ReUploadingPQC(tf.keras.layers.Layer):
+    """
+    Performs the transformation (s_1, ..., s_d) -> (theta_1, ..., theta_N, lmbd[1][1]s_1, ..., lmbd[1][M]s_1,
+        ......., lmbd[d][1]s_d, ..., lmbd[d][M]s_d) for d=input_dim, N=theta_dim and M=n_layers.
+    An activation function from tf.keras.activations, specified by `activation` ('linear' by default) is
+        then applied to all lmbd[i][j]s_i.
+    All angles are finally permuted to follow the alphabetical order of their symbol names, as processed
+        by the ControlledPQC.
+    """
+
+    def __init__(self, qubits, n_layers, observables, activation="linear", name="re-uploading_PQC"):
+        super(ReUploadingPQC, self).__init__(name=name)
+        self.n_layers = n_layers
+        self.n_qubits = len(qubits)
+
+        circuit, theta_symbols, input_symbols = generate_circuit(qubits, n_layers)
+
+        theta_init = tf.random_uniform_initializer(minval=0.0, maxval=np.pi)
+        self.theta = tf.Variable(
+            initial_value=theta_init(shape=(1, len(theta_symbols)), dtype="float32"),
+            trainable=True, name="thetas"
+        )
+
+        lmbd_init = tf.ones(shape=(self.n_qubits * self.n_layers,))
+        self.lmbd = tf.Variable(
+            initial_value=lmbd_init, dtype="float32", trainable=True, name="lambdas"
+        )
+
+        # Define explicit symbol order.
+        symbols = [str(symb) for symb in theta_symbols + input_symbols]
+        self.indices = tf.constant([symbols.index(a) for a in sorted(symbols)])
+
+        self.activation = activation
+        self.empty_circuit = tfq.convert_to_tensor([cirq.Circuit()])
+        self.computation_layer = tfq.layers.ControlledPQC(circuit, observables)        
+
+    def call(self, inputs):
+        # inputs[0] = encoding data for the state.
+        batch_dim = tf.gather(tf.shape(inputs[0]), 0)
+        tiled_up_circuits = tf.repeat(self.empty_circuit, repeats=batch_dim)
+        tiled_up_thetas = tf.tile(self.theta, multiples=[batch_dim, 1])
+        tiled_up_inputs = tf.tile(inputs[0], multiples=[1, self.n_layers])
+        scaled_inputs = tf.einsum("i,ji->ji", self.lmbd, tiled_up_inputs)
+        squashed_inputs = tf.keras.layers.Activation(self.activation)(scaled_inputs)
+
+        joined_vars = tf.concat([tiled_up_thetas, squashed_inputs], axis=1)
+        joined_vars = tf.gather(joined_vars, self.indices, axis=1)
+
+        return self.computation_layer([tiled_up_circuits, joined_vars])
+    
+class Alternating(tf.keras.layers.Layer):
+    def __init__(self, output_dim):
+        super(Alternating, self).__init__()
+        self.w = tf.Variable(
+            initial_value=tf.constant([[(1.)**i] for i in range(output_dim)]), dtype="float32",
+            trainable=True, name="obs-weights")
+            
+    def call(self, inputs):
+        return tf.math.multiply(inputs, tf.transpose(self.w))
+
+def generate_model_policy(qubits, n_layers, n_actions, observables):
+    """Generates a Keras model for a data re-uploading PQC policy."""
+
+    input_tensor = tf.keras.Input(shape=(len(qubits), ), dtype=tf.dtypes.float32, name='input')
+    re_uploading_pqc = ReUploadingPQC(qubits, n_layers, observables)([input_tensor])
+    process = tf.keras.Sequential([
+        Alternating(n_actions),
+        #tf.keras.layers.Lambda(lambda x: x * beta),
+        #tf.keras.layers.Softmax()
+    ], name="observables-policy")
+    #policy = re_uploading_pqc
+    policy = process(re_uploading_pqc)
+    model = tf.keras.Model(inputs=[input_tensor], outputs=policy)
+
+    return model
+
+def make_discriminator_model(chopsize, noise_dim):
+    model = tf.keras.Sequential()
+    model.add(layers.Dense(30, use_bias=False, input_shape=(chopsize,)))    
+    model.add(layers.LeakyReLU())             
+    model.add(layers.Dense(50))
+    model.add(layers.Dropout(0.1))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dense(50))
+    model.add(layers.Dropout(0.1))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dense(50))
+    model.add(layers.Dropout(0.1))
+    model.add(layers.LeakyReLU())
+    #model.add(layers.Dense(noise_dim_bigger))
+    model.add(layers.Dense(noise_dim))
+    model.add(layers.LeakyReLU())
+    model.add(layers.Dense(1))
+
+    return model
+
+def make_discriminator_model_conv(chopsize):
+    model = tf.keras.Sequential()
+    model.add(layers.Convolution1D(64,10,padding='same',input_shape=(chopsize,1)))
+    model.add(layers.LeakyReLU(0.2))
+    model.add(layers.Convolution1D(128,10,padding='same'))
+    model.add(layers.LeakyReLU(0.2))
+    model.add(layers.Convolution1D(128,10,padding='same'))
+    model.add(layers.LeakyReLU(0.2))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(32))
+    model.add(layers.LeakyReLU(0.2))
+    model.add(layers.Dropout(0.5))
+    model.add(layers.Dense(1))
+    return model
